40dad3a9ae73a4fd6060da57993a6d2171d67ade
Tanmay Dubey <tanmaydubey1947@gmail.com>
2024-01-21 15:06:45 +0530

    Updated code to fetch the existing topic only

--- src/main/resources/static/question-form/addQuestion/addQuestion.js
+++ src/main/resources/static/question-form/addQuestion/addQuestion.js
@@ -1,12 +1,12 @@
 function addQuestion() {
     const questionName = document.getElementById('questionName').value;
-    const topic = document.getElementById('topic').value;
+    const topicDropdown = document.getElementById('topicDropdown').value;
     const source = document.getElementById('source').value;
     const tags = document.getElementById('tags').value;
 
     const data = {
         questionName: questionName,
-        topic: topic,
+        topicDropdown: topicDropdown,
         source: source,
         tags: tags
     };
@@ -31,18 +31,37 @@ function addQuestion() {
         console.error('There was a problem adding the question:', error);
     });
 
-    showAlert('Question Addedd Successfully...');
+    showAlert('Question Added Successfully...');
     clearInputFields();
 }
 
 
 function clearInputFields() {
     document.getElementById('questionName').value = '';
-    document.getElementById('topic').value = '';
+    document.getElementById('topicDropdown').value = '';
     document.getElementById('source').value = '';
     document.getElementById('tags').value = '';
 }
 
 function showAlert(message) {
     alert(message);
-}    
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+fetchTopics();
+});
+
+ function fetchTopics() {
+        const topicDropdown = document.getElementById('topicDropdown');
+        fetch('http://localhost:8080/api/questions/getAllTopic')
+            .then(response => response.json())
+            .then(topics => {
+                topics.forEach(topic => {
+                    const option = document.createElement('option');
+                    option.value = topic;
+                    option.textContent = topic;
+                    topicDropdown.appendChild(option);
+                });
+            })
+            .catch(error => console.error('Error fetching topics:', error));
+    }
